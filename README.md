4ca1828e1f0bbab867cb21ecfc933f34bda7d356
PoroshMondal <47239915+PoroshMondal@users.noreply.github.com>
2024-04-19 17:03:43 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# TourMate
+Tour Mate is a simple application developed using Kotlin and the latest Android Components.
+
+## Branch Information
+- master
+- development
+
+## Design Pattern
+- MVVM
+- Repository pattern for local and cloud db operations
+
+## Features and components 
+- Details will add later
+
+## Binding
+- Layout binding used to bind the layout
+- Data binding used to bind the ToDo RecyclerView adapter
+
+## Database
+- Later
+
+## Background Operation
+- Later
+
+## Dependency Injection (DI)
+- Android Hilt library used to make the class independent to it's dependencies
---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.daggerHilt)
+    alias(libs.plugins.gooleServicesPlugin)
+}
+
+android {
+    namespace = "com.innovative.porosh.tourmate"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.innovative.porosh.tourmate"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+    // Allow references to generated code
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+
+    // dagger hilt for DI
+    implementation (libs.dagger.hilt.android)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    kapt (libs.hilt.compiler)
+
+    //noinspection KaptUsageInsteadOfKsp
+    //kapt(libs.androidx.room.compiler)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
